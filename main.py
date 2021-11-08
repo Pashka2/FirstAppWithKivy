802248a8e0f408fa8ecc61b24e1ae15c82b65fab
Pasha <pashasachivichik@gmail.com>
2021-11-07 20:43:37 -0500

    adding the part to call the gridlayout constructor and giving it some parameters

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 import kivy
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
 
+## will hold the design elements of the app
+class MyGrid(GridLayout):
+    def __init__(self, **kwargs):
+        super(MyGrid,self).__init__(**kwargs)
+
+
+
+## executing the app
 class MyApp(App):
 
     def build(self):
-        return Label(text = "Hello Pasha")
+        return Label(text = "Hello!")
 
 if __name__ == '__main__':
     MyApp().run()
